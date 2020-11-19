96ff93317c4afc17d8c3deb56cbc7ded7dbc7c10
Mulindwa1 <34136422+Mulindwa1@users.noreply.github.com>
2020-11-19 18:46:27 +0300

    Create index.php
    This is the first iteration of the index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset = 'utf-8'>
+    <title>Stakeholder Management System</title>
+  </head>
+  <body>
+    <header>
+      <div></div>
+    </header>
+    <section>
+      <div></div>
+    </section>
+    <footer>
+      <div></div>
+    </footer>
+  </body>
+</html>
